2db0b99b8e74a6cba03f6920fd2e9069a0765c10
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2019-11-21 21:52:27 +0530

    spelling mistake, should be connectionstring

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const counter = require("./controllers/counter");
 const db = knex({
   client: "pg",
   connection: {
-    connectString: "process.env.DATABASE_URL",
+    connectionString: "process.env.DATABASE_URL",
     ssl: true
   }
 });
